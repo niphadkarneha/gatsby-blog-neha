fc33009bdd83922dff1d65de71e22084fa56a782
Senior Dev <therapturevoid@gmail.com>
2020-07-17 07:12:26 -0700

    20202007170720
    20202007170720

--- src/App.js
+++ src/App.js
@@ -1,100 +1,50 @@
 import React, { Component } from "react"
 import logo from "./logo.svg"
 import "./App.css"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Home from './Components/Home/Home';
-import News from './Components/News/News';
-import Blog from './Components/Blog/Blog';
-import Article from './Components/Blog/Article';
-import {resumeData} from './resumeData';
-import Switch2 from 'react-input-switch';
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      homeData: resumeData.home,
-      switchChecked: false,
-      appClr: "black",
-      appBackClr: "white",
-      counter: 0,
-      switchValue2: false,
-      articles: resumeData.blog.articles
-    };
-    this.handleChange = this.handleChange.bind(this);
+
+class LambdaDemo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { loading: false, msg: null }
   }
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        ...this.state,
-        counter: this.state.counter === 10 ? 0 : this.state.counter + 1,
-      })
-    }, 3000)
+
+  handleClick = api => e => {
+    e.preventDefault()
+
+    this.setState({ loading: true })
+    fetch("/.netlify/functions/" + api)
+      .then(response => response.json())
+      .then(json => this.setState({ loading: false, msg: json.msg }))
   }
-  handleChange = (switchChecked) =>{
-    this.setState({
-      switchChecked,
-      appClr: switchChecked?"white":"black",
-      appBackClr: switchChecked?"black":"white"
-    });
-  }
-  setValue = () => {
-    var prevVal = this.state.switchValue2;
-    this.setState({
-      switchValue2: prevVal?false:true,
-      appClr: prevVal?"black":"white",
-      appBackClr: prevVal?"white":"black"
-    });
-  }
+
   render() {
-    var articleRoutes = this.state.articles.map((article, i) => {
-      var articleLink = "/" + article.link;
-      return <Route key={i} exact path={articleLink}>
-              <Article articleNo={i} articles={this.state.articles} blogHeader={resumeData.blog.header} copyright={resumeData.blog.copyright} appClr={this.state.appClr}/>
-            </Route>
-    });
+    const { loading, msg } = this.state
+
     return (
-      <Router>
-      <div className="App" style={{backgroundColor: this.state.appBackClr}}>
-      <Switch2
-          styles={{
-            track: {
-              backgroundColor: 'white'
-            },
-            trackChecked: {
-              backgroundColor: '#000'
-            },
-            button: {
-              backgroundColor: '#96979c'
-            },
-            buttonChecked: {
-              backgroundColor: '#000'
-            }
-          }}
-          on={true} off={false} value={this.state.switchValue2} onChange={this.setValue}
-        />
-        <div className="container">
-        <Switch>
-          <Route exact path="/">
-            <Home homeData={this.state.homeData} appClr={this.state.appClr} counter={this.state.counter}/>
-          </Route>
-          <Route exact path="/newsletter">
-            <News social={this.state.homeData.social} appClr={this.state.appClr} colors={this.state.homeData.colors}/>
-          </Route>
-          <Route path="/blog">
-            <Blog blogData={resumeData.blog} appClr={this.state.appClr}/>
-          </Route>
-          {articleRoutes}
-        </Switch>
-        </div>
-      </div>
-    </Router>
-    );
+      <p>
+        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
+        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
+        <br />
+        <span>{msg}</span>
+      </p>
+    )
   }
 }
 
-export default App;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <LambdaDemo />
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
