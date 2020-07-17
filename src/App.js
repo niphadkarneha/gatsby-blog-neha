99162c1a0057a68a7deda6a012596b1a82d186a7
Senior Dev <therapturevoid@gmail.com>
2020-07-17 10:34:27 -0700

    20200717-14
    20200717-14

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import News from './Components/News/News';
+import Blog from './Components/Blog/Blog';
+import Article from './Components/Blog/Article';
+import {resumeData} from './resumeData';
+import Switch2 from 'react-input-switch';
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      homeData: resumeData.home,
+      switchChecked: false,
+      appClr: "black",
+      appBackClr: "white",
+      counter: 0,
+      switchValue2: false,
+      articles: resumeData.blog.articles
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        ...this.state,
+        counter: this.state.counter === 10 ? 0 : this.state.counter + 1,
+      })
+    }, 3000)
+  }
+  handleChange = (switchChecked) =>{
+    this.setState({
+      switchChecked,
+      appClr: switchChecked?"white":"black",
+      appBackClr: switchChecked?"black":"white"
+    });
+  }
+  setValue = () => {
+    var prevVal = this.state.switchValue2;
+    this.setState({
+      switchValue2: prevVal?false:true,
+      appClr: prevVal?"black":"white",
+      appBackClr: prevVal?"white":"black"
+    });
+  }
+  render() {
+    var articleRoutes = this.state.articles.map((article, i) => {
+      var articleLink = "/" + article.link;
+      return <Route key={i} exact path={articleLink}>
+              <Article articleNo={i} articles={this.state.articles} blogHeader={resumeData.blog.header} copyright={resumeData.blog.copyright} appClr={this.state.appClr}/>
+            </Route>
+    });
+    return (
+      <Router>
+      <div className="App" style={{backgroundColor: this.state.appBackClr}}>
+      <Switch2
+          styles={{
+            track: {
+              backgroundColor: 'white'
+            },
+            trackChecked: {
+              backgroundColor: '#000'
+            },
+            button: {
+              backgroundColor: '#96979c'
+            },
+            buttonChecked: {
+              backgroundColor: '#000'
+            }
+          }}
+          on={true} off={false} value={this.state.switchValue2} onChange={this.setValue}
+        />
+        <div className="container">
+        <Switch>
+          <Route exact path="/">
+            <Home homeData={this.state.homeData} appClr={this.state.appClr} counter={this.state.counter}/>
+          </Route>
+          <Route exact path="/newsletter">
+            <News social={this.state.homeData.social} appClr={this.state.appClr} colors={this.state.homeData.colors}/>
+          </Route>
+          <Route path="/blog">
+            <Blog blogData={resumeData.blog} appClr={this.state.appClr}/>
+          </Route>
+          {articleRoutes}
+        </Switch>
+        </div>
+      </div>
+    </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/News/News.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import Social from '../Home/Social';
+class News extends Component {
+  constructor(props){
+    super(props);
+ }
+ getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+  changeColor = () => {
+    var links = document.querySelectorAll("a");
+    for (const [index, link] of links.entries()) {
+       if(link){
+          var colorVal = this.getRandomInt(4);
+          link.style.color = this.props.colors[colorVal];
+       }
+    }
+ }
+  render() {
+    this.changeColor();  
+    return (
+      <div style={{color: this.props.appClr}}>
+        <div className="header-section">
+          <div className="header-frame">
+            <h1>rendezvous with cassidoo</h1>
+            <h2>a tech newsletter for you</h2>
+          </div>
+        </div>
+        <div className="newsletter-blurb">
+          In this <span>weekly newsletter</span> I'm going to share with you
+          what's <span className="blue">new and cool</span> in the world of web
+          development, with content for <span>everyone</span>, from beginners to
+          pros. <br />
+          There's also an
+          <span className="green">interview question of the week</span>, where you can
+          test your skills and I'll share the results the following week.
+          <br />
+          I'll also send you <span>jokes</span> and
+          <span className="yellow">stories</span>.
+          <br />
+          And pics of <span className="blue">mechanical keyboards</span>.
+          <br />
+          I won't spam though, <span className="green">I promise</span>. <br />
+          You can also <a href="https://buttondown.email/cassidoo/archive">view previous issues</a>, and <a href="https://buttondown.email/cassidoo/rss" >subscribe via RSS</a>!
+        </div>
+        <div className="sneaky-sponsor">
+          Want to sponsor this newsletter? 
+          <a href="mailto:cassidy@cassidoo.co" style={{marginLeft: 10}}>Drop me a line</a>!
+        </div>
+        <Social social={this.props.social} colors={this.props.colors}/>
+        <div className="go-home">
+          <a href="/">go to nehapersonal.co</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default News;
