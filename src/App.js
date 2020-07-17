d7e97027bdc9bb4030084ebd846881ec71b03bed
Senior Dev <therapturevoid@gmail.com>
2020-07-17 08:39:35 -0700

    200839
    200839

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React, { Component } from "react"
 import logo from "./logo.svg"
 import "./App.css"
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
 
 class App extends Component {
   render() {
