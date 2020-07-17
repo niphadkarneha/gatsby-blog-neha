32a1f71d8cc2609316dc429bacbc64e8f239fe10
Senior Dev <therapturevoid@gmail.com>
2020-07-17 09:10:50 -0700

    202007170910
    202007170910

--- src/App.js
+++ src/App.js
@@ -2,28 +2,11 @@ import React, { Component } from "react"
 import logo from "./logo.svg"
 import "./App.css"
 
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        test
-      </p>
-    )
-  }
-}
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+        test
       </div>
     )
   }
