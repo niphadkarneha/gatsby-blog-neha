57c040d67a45c9c129df5f631e595a20b8eb2999
Neha <40610197+niphadkarneha@users.noreply.github.com>
2025-05-26 11:11:41 -0400

    Update resumeData.js
    Updated the json file, removed unused sections and comments

--- src/resumeData.js
+++ src/resumeData.js
@@ -8,30 +8,10 @@ export const resumeData  = {
     },
     "social": [
       {
-        "name":"twitter",
-        "url":"https://twitter.com/NiphadkarNeha",
-        "className":"fa fa-twitter"
-      },
-      // {
-      //   "name":"patreon",
-      //   "url":"https://www.patreon.com/cassidoo",
-      //   "className":"fa fa-google-plus"
-      // },
-      // {
-      //   "name":"newsletter",
-      //   "url":"/newsletter",
-      //   "className":"fa fa-linkedin"
-      // },
-      {
         "name":"github",
         "url":"https://github.com/niphadkarneha",
         "className":"fa fa-github"
       },
-      // {
-      //   "name":"codepen",
-      //   "url":"http://codepen.io/cassidoo/",
-      //   "className":"fa fa-skype"
-      // },
       {
         "name":"linkedin",
         "url":"http://linkedin.com/in/niphad",
@@ -46,25 +26,14 @@ export const resumeData  = {
         "name":"blog",
         "url":"/blog",
         "className":"fa fa-linkedin"
-      },
+      }
     ],
     "about":[
       {
         "title" : "short",
         "class": "show",
-        "content": "Hi there! I'm a Data Analyst Manager at 👩‍💻 <a href='https://www.odu.edu/acadaffairs/directory'>Old Dominion University with the Department of Academic Affairs </a>. Prior to joining ODU, I worked as a Data Analyst at MAP Communications, Inc where I worked with PowerBI, Logi Analytics, and SAP Crystal Reports."
+        "content": "Hey there! I’m Neha — a Data Analytics Manager at 👩‍💻 <a href='https://www.odu.edu/digital-technology'>Old Dominion University</a> and a proud technologist in the Division of Digital Transformation and Technology. With over a decade of experience turning data into action, I build modern, insight-rich solutions that drive smarter decision-making at my alma mater.<br /><br />I’m a visual thinker at heart — passionate about tools like Tableau and Power BI — and bring a solid foundation in legacy platforms such as Logi Analytics and SAP Crystal Reports. From modernizing reporting workflows to architecting intuitive dashboards, I thrive at the intersection of data, design, governance, and impact.<br /><br />© 2022 — Built by Neha N.<br />Big thanks to the brilliant Cassidy Williams for the design inspo — and for turning mechanical keyboards into an art form! 😄"
       }
-      //,
-      // {
-      //   "title": "long",
-      //   "content": "Well hello there! My name is Neha and I'm a Principal Developer Experience Engineer at <a href='https://www.netlify.com/?utm_source=cassidoowebsite&utm_medium=bio-cs&utm_campaign=devex'>Netlify</a>. Before now, I worked at React Training, CodePen, Amazon, L4 Digital, Clarifai, and Venmo, and graduated from Iowa State University. <br />I run <a href='http://cassidoo.co/newsletter'> a weekly newsletter</a> and love teaching and helping people become better coders! In my spare time, I love playing music, building mechanical keyboards and LEGO sets, and hanging out with <a href='https://twitter.com/cwillycs'>my sister Cami</a>. We also like to make cool things with our laser cutter and 3D printer, and have <a href='https://www.etsy.com/shop/LeakyBucketDesign'>a little Etsy shop</a> if you want to check it out! I also have a <a href='https://www.patreon.com/cassidoo'>Patreon</a> if you'd like to chat with me regularly (our Discord group is so active I can hardly keep up, come join in the fun!), get resume reviews, schedule mock interviews with me, or get a few coupons and gifts from me on occasion. No pressure to join in, and I appreciate your support a ton no matter what! <br />In terms of where I am on the internet (and off), you might have seen me in <a href='https://www.glamour.com/gallery/top-new-women-leaders-in-technology' >Glamour Magazine</a> or the <a href='https://www.amazon.com/gp/video/detail/B07L6S92HG/'>Big Dream documentary</a>a few years back, or more recently you might have seen my <a href='https://www.producthunt.com/posts/massdrop-x-hasbro-scrabble-keyboard'>Scrabble keyboard</a> project in a couple dozen different publications! I also made a successfully funded <a href='https://www.kickstarter.com/projects/cassidoo/go-on-the-go/'>Kickstarter project called Go: On the Go!</a> to get people interested in playing the game go. I've spoken at over a hundred events around the world, and have made online classes on <a href='https://www.udemy.com/user/cassidywilliams/'>Udemy</a>, <a href='https://www.skillshare.com/r/profile/Cassidy-Williams/3062729'>Skillshare</a>, and moooore. I'll (likely) happily speak at your event or podcast, or do influencer work for your org. I have a <a href='https://github.com/cassidoo/talks/blob/master/speaker-rider.md'>speaker rider</a>, if you'd like to talk to me about your event. Feel free to <a href='mailto:cassidy@cassidoo.co'>email me</a> with details! <br />Alright, since you've read this far, have a joke: Why are mountains so funny? Because they're hill areas.",
-      //   "class": ""
-      // },
-      // {
-      //   "title": "speaker",
-      //   "content": "Neha is a Principal Developer Experience Engineer at Netlify. She's worked for several other places, including CodePen, Amazon, and Venmo, and she's had the honor of working with various non-profits, including cKeys and Hacker Fund as their Director of Outreach. She's active in the developer community, and one of Glamour Magazine's 35 Women Under 35 Changing the Tech Industry and LinkedIn's Top Professionals 35 &amp; Under. As an avid speaker, Neha has participated in several events including the Grace Hopper Celebration for Women in Computing, TEDx, the United Nations, and dozens of other technical events. She wants to inspire generations of STEM students to be the best they can be, and her favorite quote is from Helen Keller: 'One can never consent to creep when one feels an impulse to soar.' She loves mechanical keyboards and karaoke.",
-      //   "class": ""
-      // }
     ],
     "colors": [
       "#10a2f5",
@@ -82,57 +51,33 @@ export const resumeData  = {
     "articles":[
       {
         "title": "Reading List",
-        "date": "Updated: February, 2022",
+        "date": "Updated: May, 2025",
         "link": "new-beginnings",
-        "short": "🙃",
+        "short": "📚",
         "header": "Books",
-        "content": `<p>Books</p>
+        "content": `<p>A curated list of books I’ve enjoyed — from digital transformation and data ethics to poetry and fiction.</p>
+
+        <h4>📘 Current Reads</h4>
         <ol>
-        <li><a href="https://www.goodreads.com/book/show/14572.Peace_Is_Every_Step">Peace Is Every Step by Thich Nhat Hanh</a></li>
-        <li><a href="https://www.goodreads.com/book/show/18003300-love-misadventure">Love & Misadventure by Lang Leav ❤️</a></li>
-        <li><a href="https://www.google.com/books/edition/Ethics_and_Data_Science/UXHKDwAAQBAJ?hl=en&gbpv=1&dq=eTHICS+AND+DATA+SCIENCE&printsec=frontcover">Ethics and Data Science a Book by DJ Patil, Hilary Mason, and Mike Loukides.</a></li>
-        <li><a href="https://aiethics.princeton.edu/case-studies/case-study-pdfs/">Case Studies by Princeton University.</a></li>
-        <li><a href="https://www.goodreads.com/book/show/22151696-lullabies">Lullabies by Lang Leav</li>
-        <li><a href="https://www.goodreads.com/book/show/22245334-the-utopia-of-rules">The Utopia of Rules: On Technology, Stupidity, and the Secret Joys of Bureaucracy by David Graeber.</li>
+          <li><a href="https://www.goodreads.com/en/book/show/55173904">Subtract: The Untapped Science of Less by Leidy Klotz</a></li>
+          <li><a href="https://www.goodreads.com/book/show/613679.The_Vegetarian">The Vegetarian by Han Kang</a></li>
+          <li><a href="https://www.goodreads.com/book/show/59808448-the-theory-of-karma">Theory of Karma by Dr. Anand Kulkarni</a></li>
+          <li><a href="https://www.goodreads.com/book/show/44128246-data-story">DataStory: Explain Data and Inspire Action Through Story by Nancy Duarte</a></li>
+          <li><a href="https://www.goodreads.com/book/show/31423262-winning-with-data">Winning with Data by Tomasz Tunguz & Frank Bien</a></li>
+          <li><a href="https://www.goodreads.com/book/show/50188288-invisible-women">Invisible Women: Data Bias in a World Designed for Men by Caroline Criado Pérez</a></li>
+          <li><a href="https://www.goodreads.com/book/show/44453636-data-feminism">Data Feminism by Catherine D’Ignazio and Lauren F. Klein</a></li>
         </ol>
-`
+
+        <h4>📚 Previous Reads</h4>
+        <ol>
+          <li><a href="https://www.goodreads.com/book/show/14572.Peace_Is_Every_Step">Peace Is Every Step by Thich Nhat Hanh</a></li>
+          <li><a href="https://www.goodreads.com/book/show/18003300-love-misadventure">Love & Misadventure by Lang Leav ❤️</a></li>
+          <li><a href="https://www.goodreads.com/book/show/22151696-lullabies">Lullabies by Lang Leav</a></li>
+          <li><a href="https://www.goodreads.com/book/show/22245334-the-utopia-of-rules">The Utopia of Rules: On Technology, Stupidity, and the Secret Joys of Bureaucracy by David Graeber</a></li>
+          <li><a href="https://aiethics.princeton.edu/case-studies/case-study-pdfs/">Case Studies by Princeton University</a></li>
+          <li><a href="https://www.google.com/books/edition/Ethics_and_Data_Science/UXHKDwAAQBAJ?hl=en&gbpv=1&dq=eTHICS+AND+DATA+SCIENCE&printsec=frontcover">Ethics and Data Science by DJ Patil, Hilary Mason, and Mike Loukides</a></li>
+        </ol>`
       }
-      // {
-      //   "title": "My Second Post!",
-      //   "date": "May 06, 2015",
-      //   "link": "my-second-post",
-      //   "short": "Wow! I love blogging so much already. Did you know that “despite its name, salted duck eggs can also be made from chicken eggs, though the…",
-      //   "content": `<p>Wow! I love blogging so much already.</p>
-      //   <p>Did you know that “despite its name, salted duck eggs can also be made from
-      //   chicken eggs, though the taste and texture will be somewhat different, and the
-      //   egg yolk will be less rich.”?
-      //   (<a href="https://en.wikipedia.org/wiki/Salted_duck_egg">Wikipedia Link</a>)</p>
-      //   <p>Yeah, I didn’t either.</p>`
-      // },
-      // {
-      //   "title": "Hello World",
-      //   "date": "May 01, 2015",
-      //   "link": "hello-world",
-      //   "short": "Hello World",
-      //   "content": `<p>This is my first post on my new fake blog! How exciting!</p>
-      //   <p>I’m sure I’ll write a lot more interesting things in the future.</p>
-      //   <p>Oh, and here’s a great quote from this Wikipedia on
-      //   <a href="https://en.wikipedia.org/wiki/Salted_duck_egg">salted duck eggs</a>.</p>
-      //   <blockquote>
-      //   <p>A salted duck egg is a Chinese preserved food product made by soaking duck
-      //   eggs in brine, or packing each egg in damp, salted charcoal. In Asian
-      //   supermarkets, these eggs are sometimes sold covered in a thick layer of salted
-      //   charcoal paste. The eggs may also be sold with the salted paste removed,
-      //   wrapped in plastic, and vacuum packed. From the salt curing process, the
-      //   salted duck eggs have a briny aroma, a gelatin-like egg white and a
-      //   firm-textured, round yolk that is bright orange-red in color.</p>
-      //   </blockquote>
-      //   <p><span class="gatsby-resp-image-wrapper" style="position: relative; display: block; margin-left: auto; margin-right: auto;  max-width: 590px;">
-      //         <a class="gatsby-resp-image-link" href=""></span>
-      //     <img class="gatsby-resp-image-image" alt="Chinese Salty Egg" title="Chinese Salty Egg" src="images/salty_egg.jpg" />
-      //     </a>
-      //       </span></p>`
-      // }
     ],
     "copyright":{
       "year": "2022",
